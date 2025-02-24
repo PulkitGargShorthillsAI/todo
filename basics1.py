5ba221e610bdbd89d1b1bf073047b87cb0f7f037
PulkitGargShorthillsAI <pulkit.garg@shorthills.ai>
2025-02-24 17:09:24 +0530

    Basics 1 done

--- basics1.py
+++ basics1.py
@@ -1,4 +1,4 @@
-
+import random
 x = 5
 y = 5.5
 z = 5j
@@ -6,3 +6,33 @@ z = 5j
 print(type(x))
 print(type(y))
 print(type(z))
+
+x = float(x)
+print(type(x))
+
+print(random.randint(0,9))
+
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0 
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+
+
+print(bool("Hello"))
+print(bool(15))
