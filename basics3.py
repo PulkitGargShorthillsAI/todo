9b18d1ffb06f1f9dcc5ac7928985788412c18467
PulkitGargShorthillsAI <pulkit.garg@shorthills.ai>
2025-02-25 15:49:54 +0530

    b3 2

--- basics3.py
+++ basics3.py
@@ -23,6 +23,67 @@ print("A \n") if a > b else print("B \n")
 
 # Truthy and Falsy
 if "":
-    print("Truthy")
+    print("Truthy \n")
 else:
-    print("Falsey")
+    print("Falsey \n")
+
+
+# Ternary operator
+x = 18 if 13 > 14 else 16
+print(x)
+
+age = 20
+status = "Adult" if age >= 18 else "Minor"
+print(status,'\n')  
+
+
+# Short Circuiting
+def check():
+    print("Function called")
+    return True
+
+print(True or check())  # Short-circuits, function not called
+print()
+
+# Logical operators
+x, y = True, False
+print(x and y)  # Output: False
+print(x or y)   # Output: True
+print(not x)    # Output: False
+
+print()
+
+# is vs ==
+x = [1, 2, 3]
+y = [1, 2, 3]
+z = x
+print(x == y)  # Output: True (same values)
+print(x is y)  # Output: False (different objects)
+print(x is z)  # Output: True (same object)
+print()
+
+# for loop
+for num in [1, 2, 3]:
+    print(num)
+print()
+
+# iterables
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print()
+
+# range
+for i in range(3):
+    print(i) 
+print()
+
+
+
+# enumerate
+fruits = ["apple", "banana", "cherry"]
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
