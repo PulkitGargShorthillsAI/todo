af1bdb30acb610c820237da8fdaf4880b6a9881e
PulkitGargShorthillsAI <pulkit.garg@shorthills.ai>
2025-02-25 10:08:21 +0530

    b2 1

---
+++ basics2.py
@@ -0,0 +1,25 @@
+#String Indexing
+str = "Hello world!!"
+print(str[0])
+print(str[-2])
+
+
+#Immutability
+# str[0] = 'h' # will give error as strings are immutable 
+
+
+#Built in methods
+print(len(str))
+print(max([1,2,3,4,5,6,7]))
+print(all([1,2,3,4,5,6,7]))
+
+
+#Booleans
+print()
+print(10 > 9)
+print(10 == 9)
+print(10 < 9) 
+
+#Lists
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
