395f38fbb183a1def527d9a85b8f4254ff561f9e
PulkitGargShorthillsAI <pulkit.garg@shorthills.ai>
2025-02-25 10:19:01 +0530

    b2 2

--- basics2.py
+++ basics2.py
@@ -23,3 +23,27 @@ print(10 < 9)
 #Lists
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+
+# Lists slicing
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a[1:4])
+print(a[0::2])
+print(a[-1::-2])
+
+
+# Matrix
+A = [[1, 4, 5, 12], 
+    [-5, 8, 9, 0],
+    [-6, 7, 11, 19]]
+
+print("A =", A) 
+print("A[1] =", A[1])      # 2nd row
+print("A[1][2] =", A[1][2])   # 3rd element of 2nd row
+print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
+
+column = [];        # empty list
+for row in A:
+  column.append(row[2])   
+
+print("3rd column =", column)
